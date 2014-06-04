c4009a1208b105143c5c503fc3ec514c8d5b0827
Carlos Castellanos <rockarlos@me.com>
2014-06-04 16:15:23 -0500

    cambio en pantallas de usuario

--- app/assets/javascripts/forma.js
+++ app/assets/javascripts/forma.js
@@ -3,38 +3,7 @@
    $('#formaforma').datepick({
     rangeSelect: true, monthsToShow: 2, showTrigger: '#calImg'});
 
-completar=function () {
 
-   console.log("auto");
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    $( "#lugar" ).autocomplete({
-      source: availableTags
-    });
-
-}
 console.log("fasdfasd");
 $.get(
     "https://api.foursquare.com/v2/venues/search?client_id=FDAPOJ2FCI2XGEQ0D5JJYXIA5M3WU332TRFN0YMDB0SIEUFN&client_secret=2GQPSIP3W5FA0KPGGUHEXTD2E20PY2HIP2FJLIT34FDQO0PG&v=20130815&ll=19.432602,-99.133205&query=museo&radius=15000",
@@ -47,4 +16,6 @@ $.get(
     
     }
 );
+
+
 
